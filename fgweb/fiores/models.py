0826b06395297db1cfe5d7f04e077868d0732087
Abdullah <abdullahzamanbabar2019@gmail.com>
2023-04-10 21:59:46 +0500

    models update --> delect method overridden to support deletion of file from the media directory when a model is deleted from the admin panel

--- fgweb/fiores/models.py
+++ fgweb/fiores/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+import os
 
 # Create your models here.
 class SliderImgs(models.Model):
     sliderImg = models.ImageField(upload_to="slider_imgs/")
+
+    def delete(self, *args, **kwargs):
+        # to delete image from the memebers folder on delete
+        os.remove(self.sliderImg.path)
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return str(self.sliderImg)
@@ -13,6 +19,11 @@ class Contact(models.Model):
     subject = models.CharField(max_length=1000)
     message = models.TextField()
     cv = models.FileField(upload_to="cvs/", max_length=300)
+
+    def delete(self, *args, **kwargs):
+        # to delete image from the memebers folder on delete
+        os.remove(self.cv.path)
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.name+"-"+self.email
@@ -33,8 +44,14 @@ class Members(models.Model):
     linkedin =  models.CharField(max_length=200)
     photo = models.ImageField(upload_to="members/")
 
+    def delete(self, *args, **kwargs):
+        # to delete image from the memebers folder on delete
+        os.remove(self.photo.path)
+        super().delete(*args, **kwargs)
+
     def __str__(self):
         return self.name+"-"+self.position
+
 
 class Projects(models.Model):
     title = models.CharField(max_length=200)
@@ -43,6 +60,13 @@ class Projects(models.Model):
     projecturl =  models.CharField(max_length=200, blank=True, verbose_name='Project Url (Optional)')
     image1 = models.ImageField(upload_to="projects/")
     image2 = models.ImageField(upload_to="projects/")
+
+    def delete(self, *args, **kwargs):
+        # to delete image from the memebers folder on delete
+        os.remove(self.logo.path)
+        os.remove(self.image1.path)
+        os.remove(self.image2.path)
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         return self.title
