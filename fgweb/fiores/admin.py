2d91709c3176c5ae039be19b45d9ebc859c17487
Abdullah <abdullahzamanbabar2019@gmail.com>
2023-04-10 22:31:13 +0500

    admin update --> overrid the delete_queryset() method to delete the related files in the media directory when multiple objects are selected and deleted from the action dropdown

--- fgweb/fiores/admin.py
+++ fgweb/fiores/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
+import os
 
 from fiores.models import SliderImgs, Contact, Members, Projects
 
 # Register your models here.
 
-admin.site.register(SliderImgs)
-admin.site.register(Contact)
-admin.site.register(Members)
-admin.site.register(Projects)
+class SliderImgsAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            os.remove(obj.sliderImg.path)
+        super().delete_queryset(request, queryset)
+
+class ContactAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        # Delete the associated file for each object in the queryset selected from the Action dropdown in admin
+        # https://docs.djangoproject.com/en/4.2/ref/contrib/admin/actions/
+        for obj in queryset:
+            os.remove(obj.cv.path)
+        # Call the parent method to delete the queryset from the database
+        super().delete_queryset(request, queryset)
+
+class MembersAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            os.remove(obj.photo.path)
+        super().delete_queryset(request, queryset)
+
+class ProjectsAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            os.remove(obj.logo.path)
+            os.remove(obj.image1.path)
+            os.remove(obj.image2.path)
+        super().delete_queryset(request, queryset)
+
+admin.site.register(SliderImgs, SliderImgsAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Members, MembersAdmin)
+admin.site.register(Projects, ProjectsAdmin)
