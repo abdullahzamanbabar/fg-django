e93e7d27ec0ebac22e0f2a16e448729f9f270560
Abdullah <abdullahzamanbabar2019@gmail.com>
2023-05-07 20:27:37 +0500

    ec2 images

--- newportfolio/helperfuns/findregionfromip.py
+++
@@ -1,5 +0,0 @@
-from ip2geotools.databases.noncommercial import DbIpCity
-
-def findregionfromip(ipaddr):
-    response = DbIpCity.get(ipaddr, api_key='free')
-    return str(response.city+", "+response.country)
--- newportfolio/portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'portfolio'
-urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    # path('projects', views.ProjectsView.as_view(), name="projects"),
-]
